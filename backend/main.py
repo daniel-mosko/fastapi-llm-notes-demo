35a0660a794bd94121a1f4b3763a0209a5d7904a
daniel-mosko <danielmosko5@gmail.com>
2025-06-16 17:37:51 +0200

    Implementation of postgres SQL, dockerfile, docker-compose, models and schemas

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,29 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from db.database import Base, SessionLocal, engine
 
 app = FastAPI()
 
 
+# Initialize DB schema
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+def read_root(db: Session = Depends(get_db)):
+    return {"message": "Connected to PostgreSQL"}
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
